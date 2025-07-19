81b47d37029c7de47f3308cc01906fe66054d362
Sachin Kumar Yadav <heysachinkry@gmail.com>
2025-07-20 00:23:08 +0530

    agent-update-delete

---
+++ src/modules/agents/ui/components/update-agent-dialog.tsx
@@ -0,0 +1,31 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { AgentForm } from "./agent-form";
+import { AgentGetOne } from "../../types";
+
+interface UpdateAgentDialogProps {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+    initialValues: AgentGetOne;
+}
+
+export const UpdateAgentDialog = ({
+    open, 
+    onOpenChange,
+    initialValues
+}: UpdateAgentDialogProps) => {
+    return (
+        <ResponsiveDialog
+            title="New Agent"
+            description="Creae New Agent"
+            open={open}
+            onOpenChange={onOpenChange}
+        >
+            <AgentForm 
+                onSucces={() => onOpenChange(false)}
+                onCancel={() => onOpenChange(false)}
+                initialValues={initialValues}
+            />
+        </ResponsiveDialog>
+    )
+}
+
